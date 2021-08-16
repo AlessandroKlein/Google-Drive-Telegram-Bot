733d65948e23a87cd6b3b64948a327cd05bfa6b5
Alessandro Klein <42942417+AlessandroKlein@users.noreply.github.com>
2021-08-16 14:56:36 -0300

    Update delete.py

--- bot/plugins/delete.py
+++ bot/plugins/delete.py
@@ -8,7 +8,7 @@ from bot import LOGGER
 def _delete(client, message):
   user_id = message.from_user.id
   if len(message.command) > 1 or message.reply_to_message:
-    sent_message = message.reply_text('🕵️**Checking Link...**', quote=True)
+    sent_message = message.reply_text('🕵️**Comprobando enlace...**', quote=True)
     if len(message.command) > 1:
       link = message.command[1]
     elif message.reply_to_message.entities[1].url:
@@ -16,7 +16,7 @@ def _delete(client, message):
     else:
       message.reply_text(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Delete[0]), quote=True)
       return
-    LOGGER.info(f'Delete:{user_id}: {link}')
+    LOGGER.info(f'Borrar:{user_id}: {link}')
     result = GoogleDrive(user_id).delete_file(link)
     sent_message.edit(result)
   else:
@@ -26,6 +26,6 @@ def _delete(client, message):
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.EmptyTrash) & CustomFilters.auth_users)
 def _emptyTrash(client, message):
   user_id = message.from_user.id
-  LOGGER.info(f'EmptyTrash: {user_id}')
+  LOGGER.info(f'Papeleravacía: {user_id}')
   msg = GoogleDrive(user_id).emptyTrash()
   message.reply_text(msg, quote=True)
